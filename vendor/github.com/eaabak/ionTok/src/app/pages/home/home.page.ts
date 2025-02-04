3dc3f8ad476b9bb2af068e11637ab350ae5f9127
Brad Alexander <Brad_Alexander@comcast.com>
2025-02-04 02:08:09 -0500

    check broswer agent

--- vendor/github.com/eaabak/ionTok/src/app/pages/home/home.page.ts
+++ vendor/github.com/eaabak/ionTok/src/app/pages/home/home.page.ts
@@ -193,7 +193,12 @@ export class HomePage implements OnInit {
     this.showHeaderDiv = window.location.host.startsWith('tikethtok.app');
     if (!this.showHeaderDiv) {
       console.log("Browser User Agent: ", navigator.userAgent);
-      alert("Account: " + window.localStorage.getItem('account'));
+      if (navigator.userAgent.includes("Linux; Android")) {
+        window.localStorage.setItem('account','droid')
+      }
+      else {
+        alert("Account: " + window.localStorage.getItem('account'));
+      }
       if (window.localStorage.getItem('account')) {
         window.sessionStorage.setItem('account',window.localStorage.getItem('account'))
         if (window.localStorage.getItem(window.localStorage.getItem('account')))
