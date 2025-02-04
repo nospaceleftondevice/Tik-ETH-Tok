ad48cebb281fbc159231d40484ef81e16d632d81
Brad Alexander <Brad_Alexander@comcast.com>
2025-02-04 01:05:54 -0500

    open in qwiky app

--- vendor/github.com/eaabak/ionTok/src/app/pages/home/home.page.ts
+++ vendor/github.com/eaabak/ionTok/src/app/pages/home/home.page.ts
@@ -192,6 +192,8 @@ export class HomePage implements OnInit {
     this.chainName = this.chainMap[chainId] || 'Unknown Chain';
     this.showHeaderDiv = window.location.host.startsWith('tikethtok.app');
     if (!this.showHeaderDiv) {
+      if (window.location.search.endsWith('qwiky'))
+        window.localStorage.setItem('account','qwiky')
       alert("Account: " + window.localStorage.getItem('account'));
       if (window.localStorage.getItem('account')) {
         window.sessionStorage.setItem('account',window.localStorage.getItem('account'))
