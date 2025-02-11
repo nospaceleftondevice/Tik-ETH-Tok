4c2b336e78a1201afb1c7f45248d435eb3e168c2
Brad Alexander <Brad_Alexander@comcast.com>
2025-02-11 03:40:02 -0500

    move camera up a bit

--- vendor/github.com/eaabak/ionTok/src/app/pages/home/home.page.ts
+++ vendor/github.com/eaabak/ionTok/src/app/pages/home/home.page.ts
@@ -395,7 +395,7 @@ export class HomePage implements OnInit {
     iframe.style.backgroundColor = 'transparent';
     iframe.style.position = 'absolute';
     iframe.style.borderRadius = '10px';
-    iframe.style.top = '180px';
+    iframe.style.top = '171px';
     iframe.style.width = '100%';
     iframe.style.height = '100%';
     iframe.style.transform = 'scale(.80)';
